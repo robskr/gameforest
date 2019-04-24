19ea85a828c311e757f08f2eae651bc26f4fe8ed
yakuthemes <info@yakuthemes.com>
2019-04-24 12:07:53 +0200

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -221,6 +221,9 @@ function compile() {
 
 function svg() {
     return gulp.src(path.src.svg)
+        .pipe(changed('./src/svg', {
+            extension: '.svg'
+        }))
         .pipe(iconfontcss({
             fontName: 'nucleo',
             path: './src/fonts/icons.sass',
