102d84ea27c15fe56af75e1bf07421a7b77695f6
yakuthemes <info@yakuthemes.com>
2019-04-24 08:10:35 +0200

    fix sass dir

--- docs/src/gulpfile.js
+++ docs/src/gulpfile.js
@@ -150,7 +150,7 @@ function browser() {
         ],
         notify: false,
         server: {
-            baseDir: '../',
+            baseDir: '../../',
             routes: {
                 "/": "../../dist"
             }
